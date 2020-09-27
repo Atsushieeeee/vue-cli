029aa5e62ac72d2bc4a16658739e9e63d364734c
Atsushi-Endo <ena-choco@live.jp>
2020-09-27 21:46:45 +0900

    練習用ページの作成

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/math',
+    name: 'Math',
+    component: () => import(/* webpackChunkName: "math" */ '../views/Math.vue')
   }
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/Math.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="math">
+    <h1>This is an Math page</h1>
+    <form action="" name="form">
+      <input type="tel" class="risoku">
+    </form>
+    <button class="mbtn">計算</button>
+  </div>
+</template>
