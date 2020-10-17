0cb2fe5b877c3327e83c68bad4921f003dcac7b1
Atsushi-Endo <ena-choco@live.jp>
2020-10-17 22:17:09 +0900

    ヘッダーのレイアウトテスト

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -1,5 +1,34 @@
 <template>
   <header>
+    <div class="header-body">
+      <div class="header-title">
+        <a href="">
+          <img src="" alt="">
+        </a>
+      </div>
+      <nav class="header-menu">
+        <ul class="header-menu-list">
+          <li>TOP</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+        </ul>
+        <div class="font-change">
+          <div class="font-change-left">
+            <h2>文字サイズ変更</h2>
+          </div>
+          <div class="font-change-right">
+            <div class="font-size">
+
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
     Header TEST
   </header>
 </template>
+
+<style>
+  
+
+</style>
